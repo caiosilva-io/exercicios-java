be6140ed9110aca6fe4e7235f6b81bb87a82ff5d
caiosilva-io <126420310+caiosilva-io@users.noreply.github.com>
2023-02-26 19:16:57 -0300

    Create contadorNumerosPares.java

---
+++ contadorNumerosPares.java
@@ -0,0 +1,32 @@
+public class SumOddRange {
+ public static boolean isOdd(int number){
+        if(number > 0){
+            if(number % 2 != 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
+
+    public static int sumOdd(int start, int end){
+        int sum = 0;
+
+        if(start > end || start <= 0 || end <= 0){
+            return -1;
+        }else{
+            for(int startLoop = start, endLoop = end; startLoop <= endLoop; startLoop++){
+                if(isOdd(startLoop)){
+                    sum += startLoop;
+                }
+            }
+            return sum;
+        }
+
+
+    }
+}
